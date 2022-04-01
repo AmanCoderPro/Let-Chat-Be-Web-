a9c5ad27476f1a034c4bf7bbad20d3ed68d9acdb
AmanCoderPro <87359434+AmanCoderPro@users.noreply.github.com>
2022-04-01 16:29:11 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBoysLM1NVYxD8yxiX8BnuSHLP_q8V5XRQ",
+    authDomain: "let-chat-be-web.firebaseapp.com",
+    projectId: "let-chat-be-web",
+    storageBucket: "let-chat-be-web.appspot.com",
+    messagingSenderId: "455013901320",
+    appId: "1:455013901320:web:ae9c57b10430177ef35dbd"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
